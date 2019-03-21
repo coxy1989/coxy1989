233678d1ee010f9f2233f9b3599431b9ebed9603
Jamie Cox <jc8478@me.com>
2019-03-21 21:33:40 +0000

    fix

--- scripts/_convert_markdown_to_html.sh
+++ scripts/_convert_markdown_to_html.sh
@@ -4,6 +4,6 @@ for filename_with_ext in $MD_FILES
 do
     split=${filename_with_ext%.md}
     filename_wo_ext=${split##*/}
-    pandoc ../site/md/"$filename_with_ext" -f markdown -t html -s -o ../site/${filename_wo_ext}.html --css ../css/markdown.css
+    pandoc ../site/md/"$filename_with_ext" -f markdown -t html -s -o ../site/${filename_wo_ext}.html --css ../css/markdown.css --columns 1000
 
 done
